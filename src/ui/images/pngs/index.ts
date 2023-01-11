2056a6a56f291e72aaa622f864310c106fb15ab0
Diego C. Silva <diegocaetano444@outlook.com>
2023-01-11 18:26:37 -0300

    added image folders

---
+++ src/ui/images/pngs/index.ts
@@ -0,0 +1,25 @@
+export { default as Marketing } from "../../../../public/icons/Marketing.png";
+export { default as Finance } from "../../../../public/icons/Finance.png";
+export { default as DigitalMarketing } from "../../../../public/icons/campanha-digital.png";
+export { default as BusinessModeling } from "../../../../public/icons/model.png";
+export { default as LeanProduction } from "../../../../public/icons/produtividade.png";
+export { default as StrategicPlanning } from "../../../../public/icons/analysis.png";
+export { default as ManagementOKRS } from "../../../../public/icons/okr.png";
+export { default as ProductivityTaskManagement } from "../../../../public/icons/time-management.png";
+export { default as IndicatorsMetrics } from "../../../../public/icons/metrics.png";
+export { default as InnovationBusinessModel } from "../../../../public/icons/business-model.png";
+export { default as QualityProductivity } from "../../../../public/icons/development.png";
+export { default as DigitalTransformation } from "../../../../public/icons/digital-transformation.png";
+
+export { default as Mindset } from "../../../../public/icons/brainstorm.png";
+export { default as Strategy } from "../../../../public/icons/strategy.png";
+export { default as Metrics } from "../../../../public/icons/metrics.png";
+export { default as Innovation } from "../../../../public/icons/innovation.png";
+
+export { default as Respect } from "../../../../public/icons/respect.png";
+export { default as OpennessChallenges } from "../../../../public/icons/challenge.png";
+export { default as Willpower } from "../../../../public/icons/forca-de-vontade.png";
+export { default as Collaboration } from "../../../../public/icons/colaboracao.png";
+export { default as CriticalThinking } from "../../../../public/icons/judgement.png";
+export { default as Commitment } from "../../../../public/icons/commitment.png";
+export { default as Dialogue } from "../../../../public/icons/speak.png";
